5db11bec7f80a68f5dcd85c0161f5741828a646a
Rendi Pradana <rendipradana4773@gmail.com>
2020-06-21 17:42:43 +0700

    add WM

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
-import InputSearch from '../InputSearch'
-import Button from '../Button'
+import React from 'react';
+import styled from 'styled-components';
+import InputSearch from '../InputSearch';
+import Button from '../Button';
 
 const Form = styled.form`
   display: flex;
-  margin-bottom: 30px;
-`
+`;
 
 const SearchForm = ({ handleSubmit, handleChange }) => {
   return (
     <Form onSubmit={handleSubmit}>
-      <InputSearch type='text' onChange={e => handleChange(e.target.value)} marginRight='10px' />
-      <Button type='submit'>Search</Button>
+      <InputSearch
+        type="text"
+        onChange={(e) => handleChange(e.target.value)}
+        marginRight="10px"
+      />
+      <Button type="submit">Search</Button>
     </Form>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
--- src/containers/ListPhotoContainer/ListPhotoContainer.js
+++ src/containers/ListPhotoContainer/ListPhotoContainer.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { IMAGES } from '../../configs'
-import SearchForm from '../../components/SearchForm'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { IMAGES } from '../../configs';
+import SearchForm from '../../components/SearchForm';
 
 const Gallery = styled.div`
   display: grid;
   grid-gap: 30px;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-auto-rows: 150px;
-`
+`;
 
 const Item = styled.div`
   width: 100%;
@@ -17,7 +17,7 @@ const Item = styled.div`
   overflow: hidden;
   box-shadow: -2px 2px 10px 0px rgba(68, 68, 68, 0.4);
   grid-row-end: span 2;
-`
+`;
 
 const Image = styled.img`
   &:hover {
@@ -27,8 +27,8 @@ const Image = styled.img`
   height: 100%;
   object-fit: cover;
   cursor: pointer;
-  transition: 0.5s transform ease-in-out
-`
+  transition: 0.5s transform ease-in-out;
+`;
 
 const Loading = styled.img`
   position: absolute;
@@ -36,40 +36,60 @@ const Loading = styled.img`
   left: 50%;
   transform: translate(-50%, -50%);
   width: 500px;
-`
+`;
+
+const Unsplash = styled.a`
+  color: blue;
+`;
+
+const Watermark = styled.p`
+  margin-top: 5px;
+  margin-bottom: 30px;
+  font-size: 10px;
+`;
 
 const ListPhoto = ({ listPhoto, actions: { getPhotos } }) => {
   const [term, setTerm] = useState('');
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    getPhotos(term)
-  }
+    getPhotos(term);
+  };
 
-  const handleChange = term => {
-    setTerm(term)
-  }
+  const handleChange = (term) => {
+    setTerm(term);
+  };
 
   const _renderPhotos = (photos) => {
     if (photos) {
-      return photos.map(photo => {
-        return <Item><Image src={photo.urls.full} alt={photo.alt_description} /></Item>
-      })
+      return photos.map((photo) => {
+        return (
+          <Item>
+            <Image src={photo.urls.full} alt={photo.alt_description} />
+          </Item>
+        );
+      });
     }
-  }
+  };
 
   return (
     <>
       <SearchForm handleSubmit={handleSubmit} handleChange={handleChange} />
+      <Watermark>
+        Powered by
+        <Unsplash href="https://unsplash.com/" target="_blank">
+          unsplash
+        </Unsplash>
+      </Watermark>
       <Gallery>
-        {listPhoto.loading ?
-          <Loading src={IMAGES.LOADING} alt='Loading' />
-          :
+        {listPhoto.loading ? (
+          <Loading src={IMAGES.LOADING} alt="Loading" />
+        ) : (
           _renderPhotos(listPhoto.photos)
-        }
+        )}
       </Gallery>
     </>
-  )
-}
+  );
+};
 
-export default ListPhoto
+export default ListPhoto;
